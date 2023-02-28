1ea1bd5e298c8b49fe8ecffe58780f8238c8f43b
erich-queiroz <erich.q.616@gmail.com>
2023-02-28 11:45:23 -0300

    melhorando responsividade, add qr code, retirando incremento e decremento de input type number

--- Front/JS/script.js
+++ Front/JS/script.js
@@ -7,9 +7,19 @@ function cadastrarDoacao() {
   let valor = valorInput.value;
   let presente = presenteInput.value;
 
+
+  
   /*while (isNaN(valor)) {
     valor = prompt("Digite apenas números para o valor da doação em PIX:");
   }*/
+  
+  // Essa váriavel removes os botões de incremento e decremento de valor dentro dos inputs do tipo number em navegadores que não suportam os psudo-elementos "::-webkit-inner-spin-button" e "::-webkit-outer-spin-button".
+  const modificarInput = document.querySelectorAll("input[type='number']");
+
+  modificarInput.forEach(function(input) {
+      input.style.WebkitAppearance = "none"; 
+      input.style.MozAppearance = "textfield";
+  });
   
   let lista = document.getElementById("lista-doacoes");
   let item = document.createElement("li");
@@ -20,6 +30,5 @@ function cadastrarDoacao() {
   valorInput.value = "";
   presenteInput.value = "";
   nomeInput.focus();
-  nomeInput.style.color = 'green';
   document.querySelector("button").textContent = "Cadastrar Outra Doação";
 }
